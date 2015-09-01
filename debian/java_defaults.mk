e725edc52e095bb9ddf6e82dbf59cbe54cd35d4d
Emmanuel Bourg <ebourg@apache.org>
2015-09-01 22:45:37 +0200

    Use OpenJDK 8 on the architectures with a port available

--- debian/java_defaults.mk
+++ debian/java_defaults.mk
@@ -3,11 +3,13 @@
 # java{,5,6,7,8,9}_architectures
 
 java9_architectures =
-java8_architectures = $(java9_architectures)
-java7_architectures = $(java8_architectures) alpha amd64 arm64 armel armhf arm64 i386 lpia \
-		mips mipsel mips64el \
+java8_architectures = $(java9_architectures) \
+		amd64 arm64 armel armhf i386 \
 		powerpc powerpcspe ppc64 ppc64el \
-		s390x sh4 x32
+		s390x x32
+java7_architectures = $(java8_architectures) \
+		alpha lpia \
+		mips mipsel mips64el sh4
 java6_architectures = $(java7_architectures)
 java5_architectures = $(java6_architectures) \
 		hppa ia64 m68k s390 sparc sparc64 \
